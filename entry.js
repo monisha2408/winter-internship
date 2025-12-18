050eb92bde183fe1adae92a15c28f490cfcb25c0
monisha <24082006moni@gmail.com>
2025-12-18 10:25:25 +0530

    Changes to secure my Mongo Environment

---
+++ entry.js
@@ -0,0 +1,91 @@
+
+const express = require("express");
+const mdb = require("mongoose");
+const Signup = require("./models/signupSchema.js");
+const bcrypt = require("bcrypt");
+const cors = require("cors")
+const dotenv = require("dotenv")
+const app = express();
+const PORT = 8001;
+
+app.use(express.json());
+app.use(cors())
+dotenv.config()
+
+mdb
+  .connect("process.env.MONGODB_URL")
+  .then(() => console.log("MongoDB Connection Successful"))
+  .catch((err) => console.log("MongoDB Connection Unsuccessful", err));
+
+app.get("/", (req, res) => {
+  res.send("Server started successfully");
+});
+
+app.post("/signup", async (req, res) => {
+  const { email, username, password } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const newSignup = new Signup({
+    email: email,
+    username: username,
+    password: hashedPassword,
+  });
+  newSignup.save();
+  res.status(200).json({ "message": "Signup Successful", "isSignup": true });
+});
+
+app.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const existingUser = await Signup.findOne({ email: email });
+    console.log(existingUser);
+
+    if (existingUser) {
+      const isValidPassword = await bcrypt.compare(
+        password,
+        existingUser.password
+      );
+      console.log(isValidPassword);
+
+      if (isValidPassword) {
+        res.status(200).json({
+          message: "Login Successful",
+          isLoggedIn: true,
+        });
+      } else {
+        res.status(401).json({
+          message: "Incorrect Password",
+          isLoggedIn: false,
+        });
+      }
+    } else {
+      res.status(404).json({
+        message: "User not Found Signup First",
+        isLoggedIn: false,
+      });
+    }
+  } catch (error) {
+    console.log("Login Error");
+    res.status(500).json({
+      message: "Login Error",
+      isLoggedIn: false,
+    });
+  }
+});
+
+app.get("/json", (req, res) => {
+  res.json({
+    College: "Sece",
+    Dept: "CYS",
+    StuCount: "64",
+  });
+});
+
+app.get("/static", (req, res) => {
+  res.sendFile(
+      "C:/Users/24082/OneDrive/Documents/seceBackend2025Dec/index.html"
+  );
+});
+
+app.listen(PORT, () => {
+  console.log(`Server Started Successfully in the port ${PORT}`);
+});
